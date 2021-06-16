687212fd66123893fb2645f55ef7c9d21643ff42
youngjun-yoo16 <yoo156@purdue.edu>
2021-06-16 10:36:21 +0900

    Updated CRUD functionality

--- app.py
+++ app.py
@@ -17,18 +17,6 @@ class BlogPost(db.Model):
     def __repr__(self):
         return 'Blog Post ' + str(self.id)
 
-all_posts = [
-	{
-		'title': 'Post 1',
-		'content': 'This is the content of post 1.',
-		'author': 'Aaron'
- 	},
- 	{
-		'title': 'Post 2',
-		'content': 'This is the content of post 2.',
- 	}
- ]
-
 @app.route('/')
 def index():
         return render_template('index.html')
@@ -38,13 +26,33 @@ def posts():
     if request.method =='POST':
         post_title = request.form['title']
         post_content = request.form['content']
-        new_post = BlogPost(title=post_title, content=post_content, author='Aaron')
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
     else:
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
     
 @app.route('/insecureposts', methods=['GET', 'POST'])
 # @app.route('/insecureposts/<string:name>', methods=['GET', 'POST'])
